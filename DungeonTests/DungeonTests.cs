6b227b4e55fdbe5da380f9444a0f8cc91013e951
Alicia Villegas <aliciavillegas1717@outlook.com>
2023-07-13 21:41:08 -0500

    DungeonTests

--- DungeonTests/DungeonTests.cs
+++ DungeonTests/DungeonTests.cs
@@ -5,9 +5,9 @@ namespace DungeonTests
         [Fact]
         public void Test1()
         {
-            Console.WriteLine("Player: ");
-            Console.Write("What is your name? ");
-            string name = "Inigo Montoya";
+            //Console.WriteLine("Player: ");
+            //Console.Write("What is your name? ");
+            //string name = "Inigo Montoya";
 
         }
     }
